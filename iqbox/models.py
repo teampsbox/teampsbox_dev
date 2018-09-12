73e980041676518dc904fec2efd0e40307749937
teampsbox <teampsbox@gmail.com>
2018-09-12 23:32:02 +0800

    Fixed iqbox models fields; added templates and static

--- iqbox/models.py
+++ iqbox/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
 class Quote(models.Model):
 	category = models.CharField(max_length=25, null=True) 
 	content = models.CharField(max_length=250, null=True)
 	quoted_by = models.CharField(max_length=50, null=True)
+	timestamp = models.DateTimeField(default=timezone.now)
 
 	def __str__(self):
-		return self.content[:80]
+		return self.content
