14372ad2ce100c97868fb9faec4375c54e50a0a8
teampsbox <teampsbox@gmail.com>
2018-09-13 11:23:28 +0800

    Fixed psblog tests.py

--- iqbox/tests.py
+++ iqbox/tests.py
@@ -16,17 +16,3 @@ class QuoteModelTest(TestCase):
 		self.assertEqual(expected_object_name, 'just a test')
 
 
-class HomePageViewTest(TestCase):    
-
-    def test_view_url_exists_at_proper_location(self):
-        resp = self.client.get('/')
-        self.assertEqual(resp.status_code, 200)
-
-    def test_view_url_by_name(self):
-        resp = self.client.get(reverse('home'))
-        self.assertEqual(resp.status_code, 200)
-
-    def test_view_uses_correct_template(self):
-        resp = self.client.get(reverse('home'))
-        self.assertEqual(resp.status_code, 200)
-        self.assertTemplateUsed(resp, 'home.html')
--- psblog/tests.py
+++ psblog/tests.py
@@ -1,3 +1,33 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
 
-# Create your tests here.
+class HomePageViewTest(SimpleTestCase):	
+	def test_home_page_status_code(self):
+		response = self.client.get('/')
+		self.assertEqual(response.status_code, 200)
+
+	def test_view_url_by_name(self):
+		response = self.client.get(reverse('home'))
+		self.assertEqual(response.status_code, 200) 
+
+	def test_view_uses_correct_template(self):
+		response = self.client.get(reverse('home'))
+		self.assertEqual(response.status_code, 200)
+		self.assertContains(response, '<h1>Homepage</h1>')
+		self.assertTemplateUsed(response, 'home.html')
+    
+
+class AboutPageViewTest(SimpleTestCase):
+	def test_about_page_status_code(self):
+		response = self.client.get('/about/')
+		self.assertEqual(response.status_code, 200)
+
+	def test_view_url_by_name(self):
+		response = self.client.get(reverse('about'))
+		self.assertEqual(response.status_code, 200)
+
+	def test_view_uses_correct_template(self):
+		response = self.client.get(reverse('about'))
+		self.assertEqual(response.status_code, 200)
+		self.assertContains(response, '<h1> About Page </h1>')
+		self.assertTemplateUsed(response, 'about.html')
