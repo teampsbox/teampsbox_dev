a1653cb0a5dabddb4c93c986c30af2d6df360030
teampsbox <teampsbox@gmail.com>
2018-09-17 09:51:11 +0800

    PSblog with forms

--- psblog/urls.py
+++ psblog/urls.py
@@ -1,10 +1,20 @@
 from django.urls import path
 
-from .views import HomePageView, AboutPageView, BlogDetailView
+from .views import (
+	HomePageView,
+	BlogDetailView,
+	BlogCreateView,
+	BlogUpdateView,
+	BlogDeleteView,
+	AboutPageView, 
+)
 
 urlpatterns = [
 	path('', HomePageView.as_view(), name='home'),
 	path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
+	path('post/new/', BlogCreateView.as_view(), name='post_new'),
+	path('post/<int:pk>/edit/', BlogUpdateView.as_view(), name='post_edit'),
+	path('post/<int:pk>/delete/', BlogDeleteView.as_view(), name='post_delete'),
 	path('about/', AboutPageView.as_view(), name='about'),
 
 ]
--- psblog/views.py
+++ psblog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 # Class-based Views
 from django.views.generic import TemplateView, ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 from .models import Post
 
@@ -14,6 +16,20 @@ class BlogDetailView(DetailView):
 	model = Post
 	template_name = 'psblog/post_detail.html'
 
+class BlogCreateView(CreateView):
+	model = Post
+	template_name = 'psblog/post_new.html'
+	fields = '__all__'
+
+class BlogUpdateView(UpdateView):
+	model = Post
+	fields = ['title', 'body']
+	template_name = 'psblog/post_edit.html'
+
+class BlogDeleteView(DeleteView):
+	model = Post
+	template_name = 'psblog/post_delete.html'
+	success_url = reverse_lazy('home')
 
 class AboutPageView(TemplateView):
 	template_name = 'psblog/about.html'
