148a2e80c5b629a1873a6b6a1f3faa566c3bb971
teampsbox <teampsbox@gmail.com>
2018-09-19 07:14:39 +0800

    Added get_object_or_404 for user

--- teampsbox/views.py
+++ teampsbox/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404
 
 from django.views.generic import TemplateView
 
 from .models import Profile
 from psblog.models import Post
+from django.contrib.auth.models import User
 
 
 class HomePageView(TemplateView):
@@ -17,5 +19,6 @@ class AboutPageView(TemplateView):
 
 @login_required
 def profile(request, username):
+    user = get_object_or_404(User, username=username)    
     user_posts = Post.objects.filter(author=request.user).order_by('-date_posted')
-    return render(request, 'teampsbox/profile.html', {'user_posts': user_posts})
+    return render(request, 'teampsbox/profile.html', {'user_posts': user_posts, 'user': user})
